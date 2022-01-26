86023edde3c86e904d28bd317536795f650a60f1
Srikanth Dorairaj <srikanth@Srikanths-MacBook-Pro.local>
2022-01-26 22:20:34 +0000

    api key in .env.local

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,9 @@
-const API_KEY = 'KwKyxfcl8E-xck4E2htmnA29997';
-export const regex = /^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/;
+export const regex =
+  /^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/;
 
 export const fetchAddress = async (postcode: string) => {
   const response = await fetch(
-    `https://api.getAddress.io/find/${postcode}?api-key=${API_KEY}`
+    `https://api.getAddress.io/find/${postcode}?api-key=${process.env.API_SECRET}`
   );
 
   return response;
